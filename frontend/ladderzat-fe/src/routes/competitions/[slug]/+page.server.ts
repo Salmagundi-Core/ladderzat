6b8d9e0d11cfc04f04275a8361968e06e6200f98
jmassink <j.massink@amsterdam.nl>
2023-09-21 14:59:55 +0200

    load mock ranking array

---
+++ frontend/ladderzat-fe/src/routes/competitions/[slug]/+page.server.ts
@@ -0,0 +1,58 @@
+import { error } from '@sveltejs/kit';
+
+interface Ranking  {
+    name: string,
+    ranking: number,
+    points: number
+}
+
+interface Competition {
+    id: number
+    name: string
+}
+
+const getCompetition = (id: number): Competition => { return {
+    id: 1,
+    name: 'Tafeltennis'}
+}
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({params}) {
+  
+    const competition: Competition = await getCompetition(params.slug);
+
+	const rankings: Ranking[] = await[{
+        name: 'Piet',
+        ranking: 1,
+        points: 160
+    },
+    {
+        name: 'Shaar',
+        ranking: 2,
+        points: 140
+    },
+    {
+        name: 'Else',
+        ranking: 3,
+        points: 130
+    },
+    {
+        name: 'Mohammed',
+        ranking: 4,
+        points: 95
+    }]
+
+
+
+	if (rankings) {
+        const data = {
+         competitionName: competition.name,
+         rankins: rankings 
+        };
+
+        return data;
+	}
+
+
+	throw error(404, 'Not found');
+}
---
+++ frontend/ladderzat-fe/src/routes/competitions/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+    console.log(data);
+</script>
+
+<h1>{data.competitionName}</h1>
+
+// Load table here
