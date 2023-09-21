34ca362742c67f02226f13a7b3da98f95d023d0a
Ramon Hofmeister <ramon@Ramons-MacBook-Pro.local>
2023-09-21 14:38:38 +0200

    adds import map

--- backend/supabase/functions/create-competition/index.ts
+++ backend/supabase/functions/create-competition/index.ts
@@ -13,8 +13,6 @@ serve(async (req) => {
     message: `Hello ${name}!`,
   }
 
-  const supabase = createClient('test', 'test');
-
   return new Response(
       JSON.stringify('test'),
       { headers: { "Content-Type": "application/json" } },
@@ -23,7 +21,7 @@ serve(async (req) => {
 })
 
 // To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/create-competition' \
 //   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
 //   --header 'Content-Type: application/json' \
 //   --data '{"name":"Functions"}'
