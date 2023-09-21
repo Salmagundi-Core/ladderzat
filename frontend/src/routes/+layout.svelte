d34c59617260167ed575ae8ff55281cab0027a5b
Harm Manders <harmmanders@gmail.com>
2023-09-21 15:06:35 +0200

    Added auth

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,26 @@
-<script>
-  import "../app.postcss";
-</script>
+<!-- src/routes/+layout.svelte -->
+<script lang="js">
+    import { invalidate } from '$app/navigation'
+    import { onMount } from 'svelte'
+    import "../app.postcss";
+  
+    export let data
+  
+    let { supabase, session } = data
+    $: ({ supabase, session } = data)
+  
+    onMount(() => {
+      const {
+        data: { subscription },
+      } = supabase.auth.onAuthStateChange((event, _session) => {
+        if (_session?.expires_at !== session?.expires_at) {
+          invalidate('supabase:auth')
+        }
+      })
+  
+      return () => subscription.unsubscribe()
+    });
+  </script>
+  
+  <slot />
 
-<slot />
